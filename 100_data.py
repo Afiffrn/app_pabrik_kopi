1de8d59d9b73f45388d1aaa9f60fb17aa7f2abd2
afiffrn <afifnugroho10@gmail.com>
2026-06-18 10:07:18 +0700

    Fix: Melatih model menggunakan data augmentasi untuk mengatasi masalah feature shortcut

--- 100_data.py
+++ 100_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import random
 
-print("🏭 Memulai Generator Data Pabrik Kopi (100 Baris) . . .")
+print("🏭 Memulai Generator Data Pabrik Kopi Versi Pintar (100 Baris) . . .")
 
 data_100 = {
     'ukuran_mm' : [],
@@ -10,22 +10,32 @@ data_100 = {
 }
 
 for i in range(100):
-    if i % 2 == 0:
-        ukuran = round(random.uniform(8.0 , 12.0), 2)
+    # 25% Data Premium Murni (Besar & Gelap)
+    if i < 25:
+        ukuran = round(random.uniform(8.0, 12.0), 2)
         gelap = round(random.uniform(60.0, 85.0), 2)
         target = 1
-    else:
+    # 25% Data Lokal Murni (Kecil & Terang)
+    elif i < 50:
         ukuran = round(random.uniform(4.0, 7.9), 2)
         gelap = round(random.uniform(10.0, 59.0), 2)
         target = 0
+    # 25% Data Anomali A: Ukuran KECIL tapi warna GELAP (Kopi gosong - Wajib Lokal!)
+    elif i < 75:
+        ukuran = round(random.uniform(4.0, 7.9), 2)
+        gelap = round(random.uniform(60.0, 85.0), 2)
+        target = 0  # <--- HUKUM JADI LOKAL
+    # 25% Data Anomali B: Ukuran BESAR tapi warna TERANG (Kopi mentah - Wajib Lokal!)
+    else:
+        ukuran = round(random.uniform(8.0, 12.0), 2)
+        gelap = round(random.uniform(10.0, 59.0), 2)
+        target = 0  # <--- HUKUM JADI LOKAL
 
-    # LURUS DI SINI! (Artinya data Premium maupun Lokal sama-sama di-append)
     data_100['ukuran_mm'].append(ukuran)
     data_100['tingkat_gelap'].append(gelap)
     data_100['target_kualitas'].append(target)
 
-# Pastikan proses simpan CSV dan Print ini berada di LUAR for-loop (paling kiri)
 df = pd.DataFrame(data_100)
 df.to_csv("data_kopi_pabrik.csv", index=False)
 
-print("✅ Sukses! File 'data_kopi_pabrik.csv' sekarang berisi 100 baris data campuran seimbang!")
+print("✅ Sukses! Datasheet baru dengan 4 variasi kelas industri berhasil dibuat!")
